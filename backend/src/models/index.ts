7658d939563b08dd817e234c6686bccaf2ebad95
Long <quoclongnguyen2003@gmail.com>
2026-07-05 23:51:40 +0700

    feat(reading): add passage models and sample seed

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -4,4 +4,6 @@ export { UserWordProgress } from './UserWordProgress';
 export { PhotoScan } from './PhotoScan';
 export { AudioTrack } from './AudioTrack';
 export { Lesson } from './Lesson';
+export { ReadingPassage } from './ReadingPassage';
+export { ReadingExplanationCache } from './ReadingExplanationCache';
 
